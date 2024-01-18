07e7779c0787da2aa10a00c3828847ebafbdb1c8
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-18 11:32:16 +0100

    Ajout des coordonées et des updates

--- TowerDefense/js/GameObject.js
+++ TowerDefense/js/GameObject.js
@@ -1,11 +1,23 @@
 export class GameObject {
     name;
     components = [];
-    update(dt){
+    x;
+    y;
 
+    constructor(name, x, y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+    update(dt){
+        for(let i=0; i<this.components.length; i++){
+            this.components[i].update(dt);
+        }
     }
 
     render(){
-        
+        for(let i=0; i<this.components.length; i++){
+            this.components[i].render();
+        }
     }
 }
