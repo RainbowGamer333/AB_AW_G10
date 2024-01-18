4f5758637531c834f3f0da49088e0e9bcf4a6d26
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-18 11:19:56 +0100

    Ajout de constantes

---
+++ TowerDefense/js/constants/Constants.js
@@ -0,0 +1,7 @@
+export class Constants {
+    static width= 640;
+    static height= 640;
+
+    static colums = 9;
+    static rows = 16;
+}
---
+++ TowerDefense/js/constants/Path.js
@@ -0,0 +1,3 @@
+export class Path {
+    static GRASS = "asset/grass.png";
+}
