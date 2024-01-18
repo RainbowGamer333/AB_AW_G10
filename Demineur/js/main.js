0fe1ee1f92aff1eca24cd8a1d112ce288eba61f4
Romain PATUREAU <romain.patureau@outlook.com>
2024-01-18 11:35:16 +0100

    Implémentation grille

--- Demineur/js/main.js
+++ Demineur/js/main.js
@@ -1,1 +1,28 @@
-//todo
+function onload() {
+    console.log("Début onload()");
+    createGrid(10, 10);
+}
+
+function createGrid(numberRows, numberColumns) {
+    let miningGrid = document.getElementById("gameBoard");
+
+    let grid = document.createElement("table");
+    grid.id = "miningGrid";
+    let tbody = document.createElement("tbody");
+
+    // Génération table
+    for (let i = 0; i < numberRows; i++) {
+        let tr = document.createElement("tr");
+
+        for (let j = 0; j < numberColumns; j++) {
+            let td = document.createElement("td");
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    grid.appendChild(tbody);
+    miningGrid.appendChild(grid);
+}
+
+
+window.addEventListener("load", onload);
