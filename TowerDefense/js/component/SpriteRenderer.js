88a4da9416481942a41c6784ece82e0c0b0d8384
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-18 13:45:29 +0100

    Ajout du sprite renderer

--- TowerDefense/js/component/SpriteRenderer.js
+++ TowerDefense/js/component/SpriteRenderer.js
@@ -1,11 +1,14 @@
-import {Component} from "./Component";
+import {Component} from "./Component.js";
+import {Constants} from "../constants/Constants.js";
 
 export class SpriteRenderer extends Component{
     texture;
+    ctx;
 
-    constructor(texture) {
+    constructor(ctx,texture) {
         super();
         this.texture = texture;
+        this.ctx = ctx;
     }
 
     update(dt){
@@ -13,6 +16,10 @@ export class SpriteRenderer extends Component{
     }
 
     render(){
+        // console.log("draw")
+        this.ctx.drawImage(this.texture, this.parent.x, this.parent.y, Constants.TILE_SIZE_ZOOMED, Constants.TILE_SIZE_ZOOMED);
 
+        // this.ctx.fillStyle = "#b66b6b"; // Couleur du carré (blanc ici)
+        // this.ctx.fillRect(this.parent.x, this.parent.y, 50, 50); // Paramètres : position x, position y, largeur, hauteur
     }
 }
