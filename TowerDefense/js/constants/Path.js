b09ccf8e96d2a928bd67ba662100533be3cafbbb
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-18 19:39:57 +0100

    Ajout de nouveaux chemins

--- TowerDefense/js/constants/Path.js
+++ TowerDefense/js/constants/Path.js
@@ -1,3 +1,15 @@
 export class Path {
-    static GRASS = "asset/grass.png";
+    //BASE
+    static BASE_PATH = "asset/";
+
+    //TILES
+    static GRASS = Path.BASE_PATH+"grass.png";
+
+    //TOWERS
+    static BASE_PATH_TOWER = Path.BASE_PATH + "/tower/";
+    static CATAPULT = Path.BASE_PATH_TOWER+"catapult.png";
+    static TOWER = Path.BASE_PATH_TOWER+"tower.png";
+    static TRIPLE_CROSSBOW = Path.BASE_PATH_TOWER+"triple_crossbow.png";
+    // static CATAPULT = Path.BASE_PATH+"/tower/catapult.png";
+    //ENEMIES
 }
