cdd7a673538fce3e62c5e98e4322d5c9ab63f492
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-23 20:41:45 +0100

    Ajout des collisions

--- games/towerDefense/js/entity/impl/projectile/Fireball.js
+++ games/towerDefense/js/entity/impl/projectile/Fireball.js
@@ -1,6 +1,10 @@
 import {Projectile} from "../../Projectile.js";
 import {Path} from "../../../constants/Path.js";
 import {SpriteRenderer} from "../../../component/SpriteRenderer.js";
+import {GameObject} from "../../../GameObject.js";
+import {Global} from "../../../constants/Global.js";
+import {Constants} from "../../../constants/Constants.js";
+import {Entity} from "../../Entity.js";
 
 export class Fireball extends Projectile{
 
@@ -14,5 +18,27 @@ export class Fireball extends Projectile{
         image.src = Path.FIREBALL;
         const spriteRenderer = new SpriteRenderer(image);
         this.addComponent(spriteRenderer)
+    }
+
+
+    update(dt) {
+        super.update(dt);
+        for (let i = 0; i < Global.gameObjects.length; i++) {
+            let gameObject = Global.gameObjects[i];
+            // console.log(this.y)
+            if (gameObject.x === this.x && gameObject instanceof Entity){ // if they are on the same line
+                if (gameObject.y + Constants.TILE_SIZE_ZOOMED >= this.y // TOP LEFT
+                    && gameObject.y <= this.y ){
+                    console.log("ZIZI");
+                    Global.removeGameObject(gameObject);
+                    Global.removeGameObject(this);
+                }
+            }
+        }
+        if (this.y <= -Constants.TILE_SIZE_ZOOMED ){
+            Global.removeGameObject(this);
+        }
+
+
     }
 }
