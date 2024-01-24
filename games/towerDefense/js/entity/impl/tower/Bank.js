67a72303115f058f4ac2860b6027fada28f5cb5a
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-24 20:52:16 +0100

    Ajout de nouvelles tours

--- games/towerDefense/js/entity/impl/tower/Bank.js
+++ games/towerDefense/js/entity/impl/tower/Bank.js
@@ -1,5 +1,30 @@
 import {Tower} from "../../Tower.js";
+import {Path} from "../../../constants/Path";
+import {SpriteRenderer} from "../../../component/SpriteRenderer";
+import {Global} from "../../../constants/Global";
 
 export class Bank extends Tower{
+    productionAmount;
+    constructor() {
+        const health = 2000;
+        const damage = 50;
+        const attackRate = 2;
 
+        let image = new Image();
+        image.src = Path.CANON;
+        const spriteRenderer = new SpriteRenderer(image);
+        super("canon", 0, 0,0, health, health, damage,attackRate);
+        this.addComponent(spriteRenderer)
+        this.productionAmount = 1;
+    }
+
+
+    update(dt) {
+        this.accumulatedTime+=dt;
+        if (this.accumulatedTime>=this.attackRate){
+            Global.coinBalance += this.productionAmount;
+            this.accumulatedTime -= this.attackRate;
+        }
+        super.update(dt);
+    }
 }
