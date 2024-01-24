d8035182fb94aa15c56d2dc05b01883519533049
LogannTille <logann.tille@gmail.com>
2024-01-24 11:02:51 +0100

    premiers ajouts
    clique fonctionnel et incrémentation du score

--- games/clicker/js/main.js
+++ games/clicker/js/main.js
@@ -1,1 +1,13 @@
-//todo
+//ici, quand le bouton est cliqué, on incrémente le score et on l'affiche
+
+document.addEventListener('DOMContentLoaded', function () {
+    var clickButton = document.getElementById('clickButton');
+    var scoreDisplay = document.getElementById('score');
+
+    var score = 0;
+
+    clickButton.addEventListener('click', function () {
+        score++;
+        scoreDisplay.textContent = 'Score: ' + score;
+    });
+});
