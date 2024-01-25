f368127db9d2e2dbb14beef42cf18290a4a3d342
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-01-25 07:36:12 +0100

    Ajout de l'annihilateur

--- games/towerDefense/js/entity/impl/tower/GoldenTree.js
+++ games/towerDefense/js/entity/impl/tower/GoldenTree.js
@@ -2,18 +2,21 @@ import {Tower} from "../../Tower.js";
 import {Path} from "../../../constants/Path.js";
 import {SpriteRenderer} from "../../../component/SpriteRenderer.js";
 import {Global} from "../../../constants/Global.js";
+import {Constants} from "../../../constants/Constants.js";
+import {Utils} from "../../../utils/Utils.js";
+import {Goblin} from "../enemy/Goblin.js";
 
 export class GoldenTree extends Tower{
     productionAmount;
     constructor() {
-        const health = 2000;
-        const damage = 50;
+        const health = 100;
+        const damage = 0;
         const attackRate = 2;
 
         let image = new Image();
         image.src = Path.GOLDEN_TREE;
         const spriteRenderer = new SpriteRenderer(image);
-        super("canon", 0, 0,0, health, health, damage,attackRate);
+        super("GoldenTree", 0, 0,0, health, health, damage,attackRate);
         this.addComponent(spriteRenderer)
         this.productionAmount = 10;
         this.attackRate = 2;
@@ -24,8 +27,16 @@ export class GoldenTree extends Tower{
         this.accumulatedTime+=dt;
         if (this.accumulatedTime>=this.attackRate){
             Global.coinBalance += this.productionAmount;
+            this.spawnGobelin();
             this.accumulatedTime -= this.attackRate;
         }
         super.update(dt);
+    }
+
+    spawnGobelin(){
+        const col = Utils.randomIntFromInterval(0,Constants.colums);
+        const entity = new Goblin();
+        entity.x = col * Constants.TILE_SIZE_ZOOMED;
+        Global.addGameObject(entity);
     }
 }
