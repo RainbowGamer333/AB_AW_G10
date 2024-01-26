0e83822073b49d0bb46128eec43b1ba28f034795
Romain PATUREAU <romain.patureau@outlook.com>
2024-01-26 10:59:52 +0100

    Implémentation timer (pas fonctionnel)

--- games/demineur/js/gameBoard.js
+++ games/demineur/js/gameBoard.js
@@ -1,7 +1,14 @@
 import { Grid } from "./grid.js";
 
+let interval;
+let time = 0;
+
 export class GameBoard {
     constructor(nbRows, nbCols, nbMines) {
+        this.gameBoard = document.getElementById("gameBoard");
+        this.grille = document.getElementById("grille");
+        this.timer = document.getElementById("gameBoardHeaderTime");
+
         this.nbRows = nbRows;
         this.nbCols = nbCols;
         this.nbMines = nbMines;
@@ -9,12 +16,59 @@ export class GameBoard {
         this.tailleCells = 30;
         this.tailleMargin = 60;
 
-        this.gameBoard = document.getElementById("gameBoard");
-        this.grille = document.getElementById("grille");
+
         this.grid = new Grid(nbRows, nbCols, nbMines);
+        this.initialiseTimer();
+        //this.startTimer();
 
         this.ajouterGrille();
         this.setStyle();
+    }
+
+    initialiseTimer() {
+        time = 0;
+        this.timerUnit = document.createElement("img");
+        this.timerTens = document.createElement("img");
+        this.timerHundreds = document.createElement("img");
+
+        this.timer.appendChild(this.timerHundreds);
+        this.timer.appendChild(this.timerTens);
+        this.timer.appendChild(this.timerUnit);
+
+        this.displayTimer();
+    }
+
+    startTimer() {
+        let interval = setInterval(this.updateTimer, 1000);
+    }
+
+    updateTimer() {
+        console.log("timer updated");
+        time++;
+        //this.displayTimer();
+    }
+
+    stopTimer() {
+        clearInterval(interval);
+    }
+
+    displayTimer() {
+        let hundreds = Math.floor(time / 100);
+        let tens = Math.floor((time - hundreds * 100) / 10);
+        let unit = time - (hundreds * 100) - (tens * 10);
+
+        this.timerHundreds.src = "./assets/timer" + hundreds + ".png";
+        this.timerTens.src = "./assets/timer" + tens + ".png";
+        this.timerUnit.src = "./assets/timer" + unit + ".png";
+    }
+
+    ajouterGrille() {
+        this.grille.appendChild(this.grid.miningGrid);
+    }
+
+    setStyle() {
+        this.setTailleCells();
+        this.setTailleGrille();
     }
 
     setTailleGrille() {
@@ -30,15 +84,5 @@ export class GameBoard {
             cells[i].style.width = this.tailleCells + "px";
             cells[i].style.height = this.tailleCells + "px";
         }
-    }
-
-    setStyle() {
-        this.setTailleCells();
-        this.setTailleGrille();
-        this.setTailleCells();
-    }
-
-    ajouterGrille() {
-        this.grille.appendChild(this.grid.miningGrid);
     }
 }
