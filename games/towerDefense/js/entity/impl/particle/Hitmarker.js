17def56bf999ee42c75a9a348cca70862659cdfc
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-02-01 11:03:06 +0100

    Ajout du hitmarker

---
+++ games/towerDefense/js/entity/impl/particle/Hitmarker.js
@@ -0,0 +1,27 @@
+import {Enemy} from "../../Enemy.js";
+import {Path} from "../../../constants/Path.js";
+import {SpriteRenderer} from "../../../component/SpriteRenderer.js";
+import {Constants} from "../../../constants/Constants.js";
+import {Utils} from "../../../utils/Utils.js";
+import {GameObject} from "../../../GameObject.js";
+import {Engine} from "../../../constants/Engine.js";
+import {Particle} from "../../Particle.js";
+
+export class Hitmarker extends Particle {
+    lifespan = 2;
+    accumulatedTime = 0.0;
+    constructor( x, y) {
+        super("Hitmaker", x, y);
+        let image = new Image();
+        image.src = Path.HITMARKER_DAMAGE;
+        const spriteRenderer = new SpriteRenderer(image);
+        this.addComponent(spriteRenderer);
+    }
+
+    update(dt) {
+        super.update(dt);
+        this.y -=3 * dt;
+    }
+
+
+}
