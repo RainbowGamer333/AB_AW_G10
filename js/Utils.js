ad987821905629528b08cc873e6b3e2887e0a9ed
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-02-07 10:18:41 +0100

    Create Utils.js

---
+++ js/Utils.js
@@ -0,0 +1,35 @@
+export class Utils {
+    static readTextFile(file, callback) {
+        let rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+
+    /**
+     * @param {String} HTML representing a single element.
+     * @param {Boolean} flag representing whether or not to trim input whitespace, defaults to true.
+     * @return {Element | HTMLCollection | null}
+     */
+    static fromHTML(html, trim = true) {
+        // Process the HTML string.
+        html = trim ? html.trim() : html;
+        if (!html) return null;
+
+        // Then set up a new template element.
+        const template = document.createElement('template');
+        template.innerHTML = html;
+        const result = template.content.children;
+
+        // Then return either an HTMLElement or HTMLCollection,
+        // based on whether the input HTML had one or more roots.
+        if (result.length === 1) return result[0];
+        return result;
+    }
+}
+
