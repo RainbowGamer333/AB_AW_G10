5125fc5b19b07c665711424e5f479e14f10a8f67
Romain PATUREAU <romain.patureau@outlook.com>
2024-02-08 10:50:17 +0100

    changement de visibilité des inputs custom en fonction du mode de jeu choisi

--- games/demineur/js/main.js
+++ games/demineur/js/main.js
@@ -1,10 +1,33 @@
 import { GameBoard } from "./gameBoard.js";
 
-const gameBoard = new GameBoard(10, 10, 10);
+const gameBoard = new GameBoard(9, 9, 10);
+const form = document.getElementById("form");
+const custom = document.getElementById("custom");
+const customW = document.getElementById("customW");
+const customH = document.getElementById("customH");
+const customM = document.getElementById("customM");
 
 function onload() {
-    let form = document.getElementById("form");
+    setupForm(form);
+}
+
+
+function setupForm(form) {
+    displayCustoms(custom.checked);
     form.addEventListener("submit", submitForm);
+
+    let diff = document.querySelectorAll('input[name="diff"]');
+    diff.forEach((radio) => {
+        radio.addEventListener('change', function() {
+            displayCustoms(this.value === "custom");
+        });
+    });
+}
+
+function displayCustoms(checked) {
+    customW.hidden = !checked;
+    customH.hidden = !checked;
+    customM.hidden = !checked;
 }
 
 
@@ -14,18 +37,18 @@ function submitForm(event) {
     let diff = document.querySelector('input[name="diff"]:checked').value;
     switch(diff) {
         case "facile":
-            gameBoard.reinitialiserGrille(9, 5, 5);
+            gameBoard.reinitialiserGrille(9, 9, 10);
             break;
         case "moyen":
-            gameBoard.reinitialiserGrille(16, 8, 15);
-            break;
-        case "difficile":
             gameBoard.reinitialiserGrille(16, 16, 40);
             break;
+        case "difficile":
+            gameBoard.reinitialiserGrille(30, 16, 99);
+            break;
         case "custom":
-            let nbCols = document.getElementById("customW").value;
-            let nbRows = document.getElementById("customH").value;
-            let nbMines = document.getElementById("customM").value;
+            let nbCols = document.querySelector('input[name="customW"]').value;
+            let nbRows = document.querySelector('input[name="customH"]').value;
+            let nbMines = document.querySelector('input[name="customM"]').value;
 
             nbCols = nbCols < 9 ? 9 : nbCols;
             nbMines = nbMines < 1 ? 1 : nbMines;
