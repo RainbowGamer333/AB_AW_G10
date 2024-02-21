1a5c5df0d298bf74abd449a9e9190e0267e33e42
Romain PATUREAU <romain.patureau@outlook.com>
2024-02-21 14:21:37 +0100

    achievements clicker

--- js/accounts.js
+++ js/accounts.js
@@ -22,6 +22,50 @@ let accounts = [
         clicker: {
             score: 100,
             pieces: 100,
+
+            achievements: [
+                {
+                    name: "Apprenti cliqueur",
+                    description: "Découvrez 1 image",
+                    obtained: false
+                },
+                {
+                    name: "Cliqueur confirmé",
+                    description: "Découvrez 5 images",
+                    obtained: false
+                },
+                {
+                    name: "Cliqueur expert",
+                    description: "Découvrez 10 images",
+                    obtained: false
+                },
+                {
+                    name: "Détective privé ou quoi ?",
+                    description: "Trouver 5 easter eggs",
+                    obtained: false
+                },
+                {
+                    name: "Artiste sous LSD",
+                    description: "Changez la couleur de fond",
+                    obtained: false
+                },
+                {
+                    name: "Contrat de footballeur",
+                    description: "Obtenir 1 million de pièces",
+                    obtained: false
+                },
+                {
+                    name: "Jeff Bezos",
+                    description: "Obtenir 1 milliard de pièces",
+                    obtained: false
+                },
+                {
+                    name: "Fou furieux",
+                    description: "Effectuer 10k cliques sans dépenser de pièces",
+                    obtained: false
+                }
+
+            ]
         },
 
         towerDefense: {
