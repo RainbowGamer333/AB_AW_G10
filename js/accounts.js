916dbc92a17c490d5e69053a0b50e6627007475f
Romain PATUREAU <romain.patureau@outlook.com>
2024-02-21 14:18:24 +0100

    Création de premier compte

---
+++ js/accounts.js
@@ -0,0 +1,31 @@
+let accounts = [
+    {
+        username: "xX_Mike_Litoris_Xx",
+        password: "test",
+        mail: "mike.litoris@gmail.com",
+        role: "user",
+
+        demineur: {
+            difficulte: {
+                facile: {
+                    score: 0
+                },
+                moyen: {
+                    score: 0
+                },
+                difficile: {
+                    score: 0
+                }
+            }
+        },
+
+        clicker: {
+            score: 100,
+            pieces: 100,
+        },
+
+        towerDefense: {
+            score: 0,
+        }
+    },
+]
