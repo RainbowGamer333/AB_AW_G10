f5407938771cfd262da8391ff40d9f5d9492920d
Romain PATUREAU <romain.patureau@outlook.com>
2024-02-26 11:18:07 +0100

    début affichage de photo d'utilisateur

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,36 @@
 // Fonction pour mettre à jour l'image de profil dans la barre de navigation
-function updateProfileImageInNavbar() {
-    const storedProfileImageUrl = getStoredProfileImage();
-    if (storedProfileImageUrl) {
-        const profileImageElement = document.getElementById("account");
-        if (profileImageElement) {
-            profileImageElement.style.backgroundImage = `url('${storedProfileImageUrl}')`;
-        } else {
-            console.log("Element with id 'account' not found.");
-        }
+
+console.log("navbar.js");
+
+document.addEventListener("DOMContentLoaded", function () {
+    let all = document.querySelectorAll("*");
+    console.log(all);
+    updateProfileImageNavbar();
+});
+
+function updateProfileImageNavbar() {
+    console.log("updateProfileImageNavbar");
+    let storedAccount = sessionStorage.getItem("account");
+    let account = document.getElementById("connexion");
+
+
+    let link = "";
+    let img;
+    if (storedAccount) {
+        console.log("Account stored");
+        img = JSON.parse(storedAccount).image;
+        link = "asset/imagesProfil/" + img;
+    } else {
+        link = "asset/icons/user.png";
     }
+    account.style.backgroundImage = `url('${link}')`;
 }
 
-// Appeler la fonction pour mettre à jour l'image de profil dans la barre de navigation
-updateProfileImageInNavbar();
+/*
+function checkSession() {
+    if (storedAccount) console.log("Session active");
+    else console.log("Session inactive");
+}
+ */
+
+//updateProfileImageNavbar();
