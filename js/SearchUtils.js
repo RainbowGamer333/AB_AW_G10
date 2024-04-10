a103e110f5c849936bb2ed401105c3b14c085506
Romain PATUREAU <romain.patureau@outlook.com>
2024-04-10 11:07:14 +0200

    remplacement de .startsWith() avec .includes()

--- js/SearchUtils.js
+++ js/SearchUtils.js
@@ -3,21 +3,18 @@ export default class SearchUtils{
     static searchGames(name){
         name = name.toLowerCase();
         let result = [];
-        for (let i=0; i< searchData.length; i++) {
-            let gameData = searchData[i];
-            if (gameData.display_name.startsWith(name)){
+        searchData.forEach(gameData => {
+            if (gameData.display_name.includes(name)){
                 result.push(gameData);
                 //TODO check aliases
             } else if (gameData.display_name_aliases.length !== 0){
-                for (let j=0; j< gameData.display_name_aliases.length; j++){
-                    const alias = gameData.display_name_aliases[j];
-                    if (alias.startsWith(name)){
+                gameData.display_name_aliases.forEach(alias => {
+                    if (alias.includes(name)){
                         result.push(gameData);
-                        // break;
                     }
-                }
+                });
             }
-        }
+        });
         return result;
     }
 }
