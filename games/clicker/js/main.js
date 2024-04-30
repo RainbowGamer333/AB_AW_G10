8fea6a4c5b54d5521e0ea07ffff73c1cc52c2507
LogannTille <logann.tille@etu.unice.fr>
2024-04-30 16:43:40 +0200

    FINI DU JEU GROS BG = ajout leaderboard fonctionnel + 0 soucis en console

--- games/clicker/js/main.js
+++ games/clicker/js/main.js
@@ -1,6 +1,6 @@
 import AchievementUtils from "../../../js/AchievementUtils.js";
 import {ScoreboardClicker} from "../../../js/Scoreboard.js";
-
+//import {initialiserScoresClicker} from "../../../js/localStorageInitialiser/scoreInitialiser.js";
 
 document.addEventListener('DOMContentLoaded', function () {
      let clickButton = document.getElementById('clickButton');
@@ -16,11 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
      let megaAutoClickCostDisplay = document.getElementById('megaAutoClickCostDisplay');
      let clickPlus1000Button = document.getElementById('clickPlus1000Button');
      let clickPlus1000CostDisplay = document.getElementById('clickPlus1000CostDisplay');
-
      let timerDisplay = document.getElementById('timerDisplay'); // Ajout de la référence au chronomètre
      let startTime;
      let timerInterval;
-
      let score = 0;
      let autoClicks = 0;
      let clickPlus1Multiplier = 1;
@@ -31,32 +29,26 @@ document.addEventListener('DOMContentLoaded', function () {
      let coins = 0;
      let cps = 0;
      let nextClickValue = clickPlus1Multiplier; // Initialisation de la prochaine valeur de clic
-
      let hasReachedButtonOne = false;
      let hasReachedButtonTwo = false;
 
-     let h4ElementsToHide = document.querySelectorAll('.h4-to-hide');
 
     const account = JSON.parse(sessionStorage.getItem("account"));
     if (account === null) window.location.href = "/AB_AW_G10/account/log-in.html";
 
     AchievementUtils.init("clicker");
-
-
+    ////////////////timer//////////////////////////
     function startTimer() {
         startTime = Date.now();
         timerInterval = setInterval(updateTimer, 1000);
     }
-
     function stopTimer() {
         clearInterval(timerInterval);
     }
-
     function updateTimer() {
          let elapsedTime = Math.floor((Date.now() - startTime) / 1000);
         displayTime(elapsedTime); // Mettre à jour l'affichage du chronomètre
     }
-
     function displayTime(time) {
          let seconds = time % 60;
          let minutes = Math.floor(time / 60) % 60;
@@ -64,6 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
         timerDisplay.textContent = 'Temps écoulé: ' + hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
     }
 
+    ////////mettre à jour les boutons sous le clicker///////
     function updateButton() {
         if (coins >= autoClickCost) {
             autoClickButton.removeAttribute('disabled');
@@ -103,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
     }
-
+////////bouton qui incrémente les scores//////
     clickButton.addEventListener('click', function () {
         // Utilise la prochaine valeur de clic pour incrémenter le score
         incrementScore(nextClickValue);
@@ -115,8 +108,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     })
 
-
-
+    /////////affichage du score//////
     function updateScore() {
         scoreDisplay.textContent = 'Score: ' + score;
         scoreDisplay.classList.add("trembling-animation");
@@ -131,10 +123,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
         updateCoins();
         updateButton();
-
-
-
-
 
         // Vérifiez si le score atteint le seuil pour chaque image et faites disparaître les rectangles correspondants
         if (score >= 2000) {
@@ -265,10 +253,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (score >= 1 && !startTime) {
             startTimer();
-        } else if (score >= 1000000000 && startTime) {
+        } else if (score >= 1100000000 && startTime) {
             stopTimer();
         }
-        console.log('Score: ' + score);
     }
 
     function updateCoins() {
@@ -277,13 +264,16 @@ document.addEventListener('DOMContentLoaded', function () {
         clickPlus1CostDisplay.textContent = 'Prochain achat de clic +1: ' + clickPlus1Cost;
         megaAutoClickCostDisplay.textContent = 'Prochain achat Mega Autoclic: ' + megaAutoClickCost;
         clickPlus1000CostDisplay.textContent = 'Prochain achat de clic +1000: ' + clickPlus1000Cost;
-        console.log("Coins: " + coins);
     }
 
     function incrementScore(amount) {
-        score += amount;
-        updateScore();
-        checkEndGame();
+        if (score < 1100000000) {
+            score += amount;
+            updateScore();
+            checkEndGame();
+        }
+
+
     }
 
     function incrementCoins(amount) {
@@ -302,8 +292,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playAutoClickAnimation(autoClicks);
+                    }
                 }, 1500);
             }
 
@@ -311,8 +303,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playAutoClickAnimation(autoClicks);
+                    }
                 }, 1200);
             }
 
@@ -320,8 +314,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playAutoClickAnimation(autoClicks);
+                    }
                 }, 1000);
             }
 
@@ -329,15 +325,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playAutoClickAnimation(autoClicks);
+                    }
                 }, 800);
             }
 
             hasReachedButtonTwo = true;
         }
     }
-
 
     function toggleMegaAutoClick() {
         if (coins >= megaAutoClickCost) {
@@ -350,16 +347,21 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playMegaAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playMegaAutoClickAnimation(autoClicks);
+                    }
+
                 }, 2000);
             }
             if (autoClicks >= 5000) {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playMegaAutoClickAnimation(autoClicks);
+                    }
                 }, 1500);
             }
 
@@ -367,8 +369,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playMegaAutoClickAnimation(autoClicks);
+                    }
                 }, 1200);
             }
 
@@ -376,8 +380,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playMegaAutoClickAnimation(autoClicks);
+                    }
                 }, 1000);
             }
 
@@ -385,10 +391,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 setInterval(function () {
                     incrementScore(autoClicks);
                     incrementCoins(autoClicks);
-                    // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
-                    playAutoClickAnimation(autoClicks);
+                    if (score <=1100000000){
+                        // Appel de la fonction pour l'animation du clic automatique avec la valeur appropriée
+                        playMegaAutoClickAnimation(autoClicks);
+                    }
                 }, 800);
+
             }
+
 
             hasReachedButtonTwo = true;
         }
@@ -415,7 +425,6 @@ document.addEventListener('DOMContentLoaded', function () {
             playClick1000Animation(amount);
         }
     }
-
 
 
     setInterval(function () {
@@ -450,8 +459,6 @@ document.addEventListener('DOMContentLoaded', function () {
         cpsDisplay.textContent = 'Clics par seconde: ' + cps;
         cps = 0;
     }, 1000);
-
-    //////////////////////////////image//////////////
 
     function playClickAnimation(amount) {
         const animationContainer = document.getElementById('animationContainer');
@@ -517,9 +524,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 2000);
     }
 
-
-
-
     // cheat code ^^
     document.addEventListener('keydown', function(event) {
         // Vérification de la séquence de touche pour le cheat
@@ -532,9 +536,6 @@ document.addEventListener('DOMContentLoaded', function () {
             AchievementUtils.increaseCounterAndTryUnlock(4, 1);
         }
     });
-
-
-
 
 //////////////////////////////image//////////////////////////
     // Fonction pour générer une couleur aléatoire au format hexadecimal
@@ -639,33 +640,31 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('clickButton').style.backgroundColor = randomColor;
     });
 
-
-
     function checkEndGame() {
         if (score >= 1100000000) {
             endGame(); // Appel à endGame() lorsque le score atteint 1 milliard
 
+
         }
     }
-
 
     function endGame() {
         // Supprimer tous les éléments du jeu
          let gameContainer = document.getElementById('gameContainer');
         gameContainer.innerHTML = '';
 
-        let endTime = new Date();
+        const endTime = new Date();
 
-        // Calculer le temps écoulé
         let elapsedTime = endTime - startTime; // Calcule le temps écoulé en millisecondes
         let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
         let minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
         let timeString = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
-
+        let elapsedTimeInSeconds = (endTime - startTime) / 1000; // Calcule le temps écoulé en secondes
+        let timeStringSecond = elapsedTimeInSeconds.toString();
 
         // Créer un conteneur pour le texte de félicitations et le bouton
-         let endGameContainer = document.createElement('div');
+        let endGameContainer = document.createElement('div');
         endGameContainer.style.textAlign = 'center';
         endGameContainer.style.display = 'flex';
         endGameContainer.style.flexDirection = 'column';
@@ -725,9 +724,6 @@ document.addEventListener('DOMContentLoaded', function () {
             imageButton.style.backgroundColor = 'lightblue';
         });
 
-
-
-
         // Ajouter le texte de félicitations et le bouton au conteneur
         endGameContainer.appendChild(congratulationsText);
         endGameContainer.appendChild(imageButton);
@@ -735,7 +731,9 @@ document.addEventListener('DOMContentLoaded', function () {
         // Ajouter le conteneur à gameContainer
         gameContainer.appendChild(endGameContainer);
 
-        this.updateScoreboard(account.username,);
+        console.log("username : " + account.username);
+        console.log("score : " + timeStringSecond);
+        ScoreboardClicker.updateScore(account.username, timeStringSecond);
     }
 
 });
